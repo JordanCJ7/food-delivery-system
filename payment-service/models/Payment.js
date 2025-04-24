82e30eaaf022ccef95d9793044d5e0c740f94bd0
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-04-25 02:57:29 +0530

    Update Payment.js
    added customerId section

--- payment-service/models/Payment.js
+++ payment-service/models/Payment.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const paymentSchema = new mongoose.Schema({
   orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
+  customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   amount: { type: Number, required: true },
-  status: { type: String, enum: ['created', 'completed', 'failed'], default: 'created' },
-  provider: { type: String, enum: ['paypal', 'payhere'], required: true },
-  providerPaymentId: String
+  method: { type: String, enum: ['paypal', 'payhere'], required: true },
+  status: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
+  transactionId: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Payment', paymentSchema);
