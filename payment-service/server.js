fffdaa4b8778303e0bb370ff230827cec70fc1aa
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-04-25 02:23:38 +0530

    Update server.js
    Implement server.js

---
+++ payment-service/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+const paymentRoutes = require('./routes/paymentRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/payments', paymentRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => console.log(`Payment Service running on port ${PORT}`));
